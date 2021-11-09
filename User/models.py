580cfcd942e2e1ab39036b1f5846e2c1fd1f0fb7
ankur <ankur.mishra@zopper.com>
2021-11-09 10:38:08 +0530

    update model

--- User/models.py
+++ User/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
     name=models.CharField(max_length=11)
     designation=models.CharField(max_length=15)
+    address=models.CharField(max_length=11,null=True)
 
     def __str__(self):
         return self.name
